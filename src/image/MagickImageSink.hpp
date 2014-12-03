a437a47341ef2b6d891f9cc33c4ef9a5148dda6c
Vanya Sergeev <vsergeev@gmail.com>
2014-12-03 04:16:10 -0800

    move image Orientation enum class into MagickImageSink class

--- src/image/MagickImageSink.hpp
+++ src/image/MagickImageSink.hpp
@@ -4,12 +4,13 @@
 #include <string>
 
 #include "ImageSink.hpp"
-#include "Orientation.hpp"
 
 namespace Image {
 
 class MagickImageSink : public ImageSink {
   public:
+    enum class Orientation { Horizontal, Vertical };
+
     MagickImageSink(std::string path, unsigned int width, Orientation orientation);
 
     virtual void append(const std::vector<uint32_t> &pixels);
