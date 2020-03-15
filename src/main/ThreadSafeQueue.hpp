450deec529b96db36efe865c99c1132e5114a617
Vanya A. Sergeev <v@sergeev.io>
2020-03-15 02:02:13 -0500

    main/ThreadSafeQueue: suffix private members with underscore

--- src/main/ThreadSafeQueue.hpp
+++ src/main/ThreadSafeQueue.hpp
@@ -18,45 +18,45 @@ class ThreadSafeQueue {
     bool wait(const std::chrono::duration<Rep, Period> &rel_time);
 
   private:
-    std::mutex lock;
-    std::condition_variable cvNotEmpty;
-    std::queue<T> queue;
+    std::mutex _lock;
+    std::condition_variable _cvNotEmpty;
+    std::queue<T> _queue;
 };
 
 template <typename T>
 size_t ThreadSafeQueue<T>::count() {
-    std::lock_guard<std::mutex> lg(lock);
-    return queue.size();
+    std::lock_guard<std::mutex> lg(_lock);
+    return _queue.size();
 }
 
 template <typename T>
 bool ThreadSafeQueue<T>::empty() {
-    std::lock_guard<std::mutex> lg(lock);
-    return queue.empty();
+    std::lock_guard<std::mutex> lg(_lock);
+    return _queue.empty();
 }
 
 template <typename T>
 void ThreadSafeQueue<T>::push(T value) {
-    std::lock_guard<std::mutex> lg(lock);
-    queue.push(value);
-    cvNotEmpty.notify_one();
+    std::lock_guard<std::mutex> lg(_lock);
+    _queue.push(value);
+    _cvNotEmpty.notify_one();
 }
 
 template <typename T>
 T ThreadSafeQueue<T>::pop() {
-    std::unique_lock<std::mutex> lg(lock);
-    while (queue.empty())
-        cvNotEmpty.wait(lg);
-    T value(std::move(queue.front()));
-    queue.pop();
+    std::unique_lock<std::mutex> lg(_lock);
+    while (_queue.empty())
+        _cvNotEmpty.wait(lg);
+    T value(std::move(_queue.front()));
+    _queue.pop();
     return value;
 }
 
 template <typename T>
 template <typename Rep, typename Period>
 bool ThreadSafeQueue<T>::wait(const std::chrono::duration<Rep, Period> &rel_time) {
-    std::unique_lock<std::mutex> lg(lock);
-    while (queue.empty())
-        return (cvNotEmpty.wait_for(lg, rel_time) == std::cv_status::no_timeout);
+    std::unique_lock<std::mutex> lg(_lock);
+    while (_queue.empty())
+        return (_cvNotEmpty.wait_for(lg, rel_time) == std::cv_status::no_timeout);
     return true;
 }
